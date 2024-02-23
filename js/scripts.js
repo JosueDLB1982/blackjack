7b22f616a8837a5cf9e49f71483652b0913a3373
Josue Lopez <josuedlopez1982@gmail.com>
2024-02-22 23:16:23 -0300

    nueva prueba

--- js/scripts.js
+++ js/scripts.js
@@ -83,7 +83,7 @@
   // Función para crear las imagenes de las cartas
   const createCards = (card, turn) => {
     const imgCard = document.createElement("img"); // creo una imagen
-    imgCard.src = `../assets/cards/${card}.webp`; // le doy el src
+    imgCard.src = `assets/cards/${card}.webp`; // le doy el src
     imgCard.classList.add("card"); // le aplico la clase
     imgCard.alt = `image ${card}`;
     displayCards[turn].append(imgCard); // inserto la img en el HTML
